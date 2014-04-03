2502534aeac6e18fed8b57447dbb55e158433bf3
William Budington <bill@eff.org>
2014-04-03 13:27:30 -0700

    Adding links to success rate and last error/failure

--- js/views/legislator.js
+++ js/views/legislator.js
@@ -3,12 +3,15 @@ define([
   'backbone',
   'mustache',
   'querystring',
+  'config',
   'text!templates/legislator.html'
-], function($, Backbone, Mustache, qs, legislatorTemplate){
+], function($, Backbone, Mustache, qs, config, legislatorTemplate){
   var LegislatorView = Backbone.View.extend({
     render: function () {
       console.log(this.options);
-      $('.legislator-container').html(Mustache.render(legislatorTemplate, this.model.attributes));
+      $('.legislator-container').html(
+        Mustache.render(legislatorTemplate, _.extend({congress_forms_server: config.CONTACT_CONGRESS_SERVER}, this.model.attributes))
+      );
     }
   })
   return LegislatorView;
