6fa747985b87df7ef36e5eda579980d931a45953
William Budington <bill@eff.org>
2014-04-04 18:23:19 -0700

    Adding the 'run_at' time to last error/failure

--- js/views/last_problem.js
+++ js/views/last_problem.js
@@ -2,12 +2,14 @@ define([
   'jquery',
   'backbone',
   'mustache',
-  'config'
+  'config',
+  'moment'
 ], function($, Backbone, Mustache, config){
   var LastProblemView = Backbone.View.extend({
     render: function () {
       console.log(this.model.attributes);
       if('error' in this.model.attributes){
+        $('#error_status').html(moment(this.model.attributes.run_at).format('MMMM Do YYYY, h:mm:ss a'));
         $('#error_info').html(this.model.attributes.error).show();
       } else if('status' in this.model.attributes && this.model.attributes.status == "error"){
         $('#error_status').html(this.model.attributes.message);
