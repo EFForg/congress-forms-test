06f7a728c44b9902a3ed3ef7c50648bbf0962a4e
Thomas Davis <thomasalwyndavis@gmail.com>
2014-04-08 14:15:19 +1000

    hash tag

--- js/lib/querystring.js
+++ js/lib/querystring.js
@@ -5,8 +5,10 @@ define({
     for(var i = 0; i < hashes.length; i++)
     {
       hash = hashes[i].split('=');
-      vars.push(hash[0]);
-      vars[hash[0]] = hash[1].indexOf('#') === -1 ? hash[1] : hash[1].substr(0, hash[1].indexOf('#'));
+      if(hash[1]) {
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1].indexOf('#') === -1 ? hash[1] : hash[1].substr(0, hash[1].indexOf('#'));
+      }
     }
     return vars;
   }
