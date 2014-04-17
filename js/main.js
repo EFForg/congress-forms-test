71fe39cb26382ac994451c29e726c2bb81f246b1
Eric Mill <eric@konklone.com>
2014-04-17 17:58:03 -0400

    Makes requests over https where possible

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ require.config({
     jquery: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery.min',
     lodash: 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.min',
     underscore: 'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min',
-    backbone: 'http://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min',
+    backbone: 'https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min',
     mustache: 'https://cdnjs.cloudflare.com/ajax/libs/mustache.js/0.7.2/mustache.min',
     text: 'https://cdnjs.cloudflare.com/ajax/libs/require-text/2.0.10/text',
     marked: 'https://cdnjs.cloudflare.com/ajax/libs/marked/0.3.1/marked.min',
@@ -48,7 +48,7 @@ require([
     qs,
     Events,
     _,
-    jsyaml, 
+    jsyaml,
     marked,
     Data,
     fancybox,
@@ -67,12 +67,12 @@ require([
     legislatorStatusRowTemplate
   ){
   console.log(qs.get());
-  // Get the legislator id from query string 
+  // Get the legislator id from query string
   var bioguide_id = qs.get().bioguide_id || '';
 
   // Bring in Example data from YAML file
   $.ajax({
-    url: 'http://corsgithub.herokuapp.com/unitedstates/contact-congress/master/support/variables.yaml',
+    url: 'https://corsgithub.herokuapp.com/unitedstates/contact-congress/master/support/variables.yaml',
     success: function (exampleYaml) {
       var exampleData = jsyaml.load(exampleYaml);
       var examples = [];
@@ -166,7 +166,7 @@ require([
           success: function (legislator_obj) {
             cb(null, legislator_obj.results);
             return;
-          } 
+          }
         });
       }
     }, function(err, results){
@@ -219,7 +219,7 @@ require([
 
 
 
-/* 
+/*
 var helper = {
   dc_zips: [20001, 20002, 20003, 20004, 20005, 20006, 20007, 20008, 20009, 20010, 20011, 20012, 20015, 20016, 20017, 20018, 20019, 20020, 20024, 20032, 20036, 20037, 20045, 20052, 20053, 20057, 20064, 20202, 20204, 20228, 20230, 20240, 20245, 20260, 20307, 20317, 20319, 20373, 20390, 20405, 20418, 20427, 20506, 20510, 20520, 20535, 20540, 20551, 20553, 20560, 20565, 20566, 20593],
   zip_in_dc: function(zip){
