e775505c50aa2c01e6ffd55e046371f7dc22d21f
William Budington <bill@eff.org>
2014-08-22 13:41:32 -0700

    Provide dj_id to template

--- js/views/fill_attempts.js
+++ js/views/fill_attempts.js
@@ -18,7 +18,8 @@ define([
           time: moment(fill_attempt.attributes.run_at).format('MMMM Do YYYY, h:mm:ss a'),
           uid: x,
           tr_class: ++x % 2 == 0 ? "active" : "",
-          screenshot_url: fill_attempt.attributes.screenshot
+          screenshot_url: fill_attempt.attributes.screenshot,
+          dj_id: fill_attempt.attributes.dj_id
         }, fill_attempt.attributes);
         if(fill_attempt.attributes.status == "error" || fill_attempt.attributes.status == "failure"){
           return Mustache.render(fillAttemptErrorTemplate, vals);
