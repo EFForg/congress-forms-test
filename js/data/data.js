d818c6eec2bfa9107c6b773a13cc43f457175173
William Budington <bill@eff.org>
2014-09-04 17:18:02 -0700

    changing data url

--- js/data/data.js
+++ js/data/data.js
@@ -1,5 +1,5 @@
 define([
-  'https://raw.github.com/sinak/congress-zip-plus-four/master/legislators.json',
+  'https://rawgit.com/sinak/congress-zip-plus-four/master/legislators.json',
 ], function(legislators) {
   return {
     legislators: legislators
