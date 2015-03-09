a39de71c281bb317657588066ba49609627eb0ce
William Budington <bill@eff.org>
2015-03-09 13:24:14 -0700

    Legislators is not jsonp, just load as text and parse json

--- js/data/data.js
+++ js/data/data.js
@@ -1,9 +1,9 @@
 define([
-  'https://rawgit.com/EFForg/congress-zip-plus-four/master/legislators.json',
+  'text!https://rawgit.com/EFForg/congress-zip-plus-four/master/legislators.json',
 ], function(legislators) {
   console.log('testing')
   console.log(legislators);
   return {
-    legislators: legislators
+    legislators: JSON.parse(legislators)
     }
 });
