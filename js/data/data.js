555ea932105d4a90a80a444d3a12662987c0e57c
William Budington <bill@eff.org>
2015-03-09 13:24:36 -0700

    Remove console.log cruft and indentation

--- js/data/data.js
+++ js/data/data.js
@@ -1,9 +1,7 @@
 define([
   'text!https://rawgit.com/EFForg/congress-zip-plus-four/master/legislators.json',
 ], function(legislators) {
-  console.log('testing')
-  console.log(legislators);
   return {
     legislators: JSON.parse(legislators)
-    }
+  }
 });
