52078aac5be588d970e5b571ca7f699c852d67d3
William Budington <bill@eff.org>
2015-03-17 11:56:47 -0700

    Load pretty-printed jobs into CodeMirror javascript widget

--- js/models/job.js
+++ js/models/job.js
@@ -1,8 +1,13 @@
 define([
+  'config',
   'backbone',
   'moment'
-], function(Backbone, moment) {
+], function(config, Backbone, moment) {
   var JobModel = Backbone.Model.extend({
+    url: function(){
+      return config.CONTACT_CONGRESS_SERVER + '/job-details/' + this.id + '?debug_key=' + config.DEBUG_KEY
+    },
+
     parse: function(resp){
       this.created_at = moment(resp.created_at).format('MMMM Do YYYY, h:mm:ss a');
       this.updated_at = moment(resp.updated_at).format('MMMM Do YYYY, h:mm:ss a');
